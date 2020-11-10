fbcd6dbc2c65516349d9576e8c37e5169d65cee4
Ezra Peisach <epeisach@mit.edu>
2020-11-10 15:29:52 -0500

    Azure integration and auto testing (#1)
    * pep8, pylint, black  cleanup
    
    * Azure integration 
    
    Co-authored-by: Ezra Peisach <ezra.peisach@rcsb.org>

--- setup.py
+++ setup.py
@@ -16,24 +16,22 @@ from setuptools.command.build_ext import build_ext
 
 
 class CMakeExtension(Extension):
-
-    def __init__(self, name, sourcedir='', sources=[]):
+    def __init__(self, name, sourcedir="", sources=None):
+        sources = sources if sources else []
         Extension.__init__(self, name, sources=sources)
         self.sourcedir = os.path.abspath(sourcedir)
 
 
 class CMakeBuild(build_ext):
-
     def run(self):
         try:
-            out = subprocess.check_output(['cmake', '--version'])
+            out = subprocess.check_output(["cmake", "--version"])
         except OSError:
-            raise RuntimeError("CMake must be installed to build the following extensions: " +
-                               ", ".join(e.name for e in self.extensions))
+            raise RuntimeError("CMake must be installed to build the following extensions: " + ", ".join(e.name for e in self.extensions))
 
         if platform.system() == "Windows":
-            cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)', out.decode()).group(1))
-            if cmake_version < '3.1.0':
+            cmakeVersion = LooseVersion(re.search(r"version\s*([\d.]+)", out.decode()).group(1))
+            if cmakeVersion < "3.1.0":
                 raise RuntimeError("CMake >= 3.1.0 is required on Windows")
 
         for ext in self.extensions:
@@ -42,99 +40,110 @@ class CMakeBuild(build_ext):
     def build_extension(self, ext):
 
         #
+        debug = True
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
-        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                      '-DPYTHON_EXECUTABLE=' + sys.executable]
+        cmakeArgs = ["-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + extdir, "-DPYTHON_EXECUTABLE=" + sys.executable]
 
-        # we need to help cmake find the correct python for this virtual env -
-        if hasattr(sys, 'real_prefix'):
-            lsp = os.path.join(sys.real_prefix, 'lib', 'libpython') + "*"
-            isp = os.path.join(sys.real_prefix, 'include', 'python') + "%s.%s" % (sys.version_info.major, sys.version_info.minor) + "*"
-        else:
-            lsp = os.path.join(sys.exec_prefix, 'lib', 'libpython') + "*"
-            isp = os.path.join(sys.exec_prefix, 'include', 'python') + "%s.%s" % (sys.version_info.major, sys.version_info.minor) + "*"
-        #
+        # We need to help cmake find the correct python for this virtual env -
+        # ---
+        libPath = None
+        lsp = os.path.join(sys.exec_prefix, "lib", "libpython") + "*"
         lpL = glob.glob(lsp)
-        if len(lpL):
-            lp = lpL[0]
-            cmake_args += ['-DPYTHON_LIBRARY=' + lp]
-
+        if lpL:
+            libPath = lpL[0]
+        elif hasattr(sys, "base_exec_prefix"):
+            lsp = os.path.join(sys.base_exec_prefix, "lib", "libpython") + "*"  # pylint: disable=no-member
+            lpL = glob.glob(lsp)
+            if lpL:
+                libPath = lpL[0]
+        if libPath:
+            cmakeArgs += ["-DPYTHON_LIBRARY=" + libPath]
+        else:
+            print("------ WARNING could not locate python library")
+        # ---
+        inclPath = None
+        isp = os.path.join(sys.exec_prefix, "include", "python") + "%s.%s" % (sys.version_info.major, sys.version_info.minor) + "*"
         ipL = glob.glob(isp)
-        if len(ipL):
-            ip = ipL[0]
-            cmake_args += ['-DPYTHON_INCLUDE_DIR=' + ip]
-        #
-        cfg = 'Debug' if self.debug else 'Release'
-        build_args = ['--config', cfg]
+        if ipL:
+            inclPath = ipL[0]
+        elif hasattr(sys, "base_exec_prefix"):
+            isp = os.path.join(sys.base_exec_prefix, "include", "python") + "%s.%s" % (sys.version_info.major, sys.version_info.minor) + "*"  # pylint: disable=no-member
+            ipL = glob.glob(isp)
+            if ipL:
+                inclPath = ipL[0]
+        if inclPath:
+            cmakeArgs += ["-DPYTHON_INCLUDE_DIR=" + inclPath]
+        else:
+            print("------ WARNING could not locate python include files")
+        # ---
+        cfg = "Debug" if self.debug else "Release"
+        buildArgs = ["--config", cfg]
 
         if platform.system() == "Windows":
-            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
-            if sys.maxsize > 2**32:
-                cmake_args += ['-A', 'x64']
-            build_args += ['--', '/m']
+            cmakeArgs += ["-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}".format(cfg.upper(), extdir)]
+            if sys.maxsize > 2 ** 32:
+                cmakeArgs += ["-A", "x64"]
+            buildArgs += ["--", "/m"]
         else:
-            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
-            build_args += ['--', '-j2']
+            cmakeArgs += ["-DCMAKE_BUILD_TYPE=" + cfg]
+            buildArgs += ["--", "-j2"]
 
         env = os.environ.copy()
-        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
-                                                              self.distribution.get_version())
-        env['RUN_FROM_DISUTILS'] = "yes"
+        env["CXXFLAGS"] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get("CXXFLAGS", ""), self.distribution.get_version())
+        env["RUN_FROM_DISUTILS"] = "yes"
         #
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
         #
-        if False:
-            print("------------------------------")
+        if debug:
+            print("------------- setup.py -----------------")
             print("Extension source path ", ext.sourcedir)
-            print("CMAKE_ARGS ", cmake_args)
+            print("CMAKE_ARGS ", cmakeArgs)
             print("self.build_temp ", self.build_temp)
             print("extdir", extdir)
             print("ext.name", ext.name)
             print("sys.executable", sys.executable)
             print("sys.exec_prefix", sys.exec_prefix)
-            print("CXXFLAGS ", env['CXXFLAGS'])
+            print("CXXFLAGS ", env["CXXFLAGS"])
 
         #
-        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
-        subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
+        subprocess.check_call(["cmake", ext.sourcedir] + cmakeArgs, cwd=self.build_temp, env=env)
+        subprocess.check_call(["cmake", "--build", "."] + buildArgs, cwd=self.build_temp)
 
 
 packages = []
-thisPackage = 'wwpdb.utils.align'
-#requires = ['future']
+thisPackage = "wwpdb.utils.align"
 requires = []
 
 
-with open('wwpdb/utils/align/__init__.py', 'r') as fd:
-    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
-                        fd.read(), re.MULTILINE).group(1)
+with open("wwpdb/utils/align/__init__.py", "r") as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE).group(1)
 
 if not version:
-    raise RuntimeError('Cannot find version information')
+    raise RuntimeError("Cannot find version information")
 
 setup(
     name=thisPackage,
     version=version,
-    description='Alignment Library and Tools',
+    description="Alignment Library and Tools",
     long_description="See:  README.md",
-    author='John Westbrook',
-    author_email='john.westbrook@rcsb.org',
-    url='http://github.com/wwpdb/py-wwpdb_utils_align',
+    author="John Westbrook",
+    author_email="john.westbrook@rcsb.org",
+    url="http://github.com/wwpdb/py-wwpdb_utils_align",
     #
-    license='Apache 2.0',
+    license="Apache 2.0",
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        "Development Status :: 3 - Alpha",
         # 'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     # entry_points={
     #    'console_scripts': [
@@ -142,32 +151,25 @@ setup(
     #    ]
     # },
     #
-    install_requires=['future', 'six'],
-    packages=find_packages(exclude=['wwpdb.utils.tests-align', 'tests.*']),
+    install_requires=["future", "six"],
+    packages=find_packages(exclude=["wwpdb.utils.tests-align", "tests.*"]),
     package_data={
         # If any package contains *.md or *.rst ...  files, include them:
-        '': ['*.md', '*.rst', "*.txt", "*.h", "*.C", ".c", "*.cpp"],
+        "": ["*.md", "*.rst", "*.txt", "*.h", "*.C", ".c", "*.cpp"],
     },
     #
-
     #
     test_suite="wwpdb.utils.tests-align",
-    tests_require=['tox'],
+    tests_require=["tox"],
     #
     # Not configured ...
     extras_require={
-        'dev': ['check-manifest'],
-        'test': ['coverage'],
+        "dev": ["check-manifest"],
+        "test": ["coverage"],
     },
     # Added for
-    command_options={
-        'build_sphinx': {
-            'project': ('setup.py', thisPackage),
-            'version': ('setup.py', version),
-            'release': ('setup.py', version)
-        }
-    },
-    ext_modules=[CMakeExtension('wwpdb.utils.align.alignlib')],
+    command_options={"build_sphinx": {"project": ("setup.py", thisPackage), "version": ("setup.py", version), "release": ("setup.py", version)}},
+    ext_modules=[CMakeExtension("wwpdb.utils.align.alignlib")],
     cmdclass=dict(build_ext=CMakeBuild),
     zip_safe=False,
 )
--- wwpdb/utils/tests-align/PairwiseAlignTests.py
+++ wwpdb/utils/tests-align/PairwiseAlignTests.py
@@ -15,48 +15,46 @@ Test cases for pairwise sequence alignment wrapper class.
 import logging
 import unittest
 
-from wwpdb.utils.align.alignlib import PairwiseAlign
+from wwpdb.utils.align.alignlib import PairwiseAlign  # pylint: disable=no-name-in-module,import-error
 from wwpdb.utils.align.SequenceExamples import SequenceExamples
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s]-%(module)s.%(funcName)s: %(message)s')
+logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s]-%(module)s.%(funcName)s: %(message)s")
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
 class PairwiseAlignTests(unittest.TestCase):
-
     def setUp(self):
         self.__verbose = True
         sE = SequenceExamples()
-        self.seqRef3L = sE.getRefSequence3List('A')
-        self.seqAuth3L = sE.getAuthSequenceList('A')
+        self.seqRef3L = sE.getRefSequence3List("A")
+        self.seqAuth3L = sE.getAuthSequenceList("A")
         #
         # Test sequence with random insertions and deletions
         #
         self.sTests = {}
-        for tt in ['T1', 'T2', 'T3', 'T4', 'T5']:
-            self.sTests[tt] = sE.getAuthSequenceListTest('A')
+        for tt in ["T1", "T2", "T3", "T4", "T5"]:
+            self.sTests[tt] = sE.getAuthSequenceListTest("A")
         #
 
     def tearDown(self):
         pass
 
     def testAlign1(self):
-        """ Run internal alignment test embedded in the class -  This is a basic santity check.
-        """
-        logger.info(" -------------------------\n")
+        """Run internal alignment test embedded in the class -  This is a basic santity check."""
+        logger.info(" -------------------------")
         try:
             pA = PairwiseAlign()
             pA.testExample()
         except Exception as e:
-            logger.exception("Failing with %s" % str(e))
+            logger.exception("Failing with %s", str(e))
             self.fail()
 
     def testAlign2(self):
-        """ Run author vs reference sequence alignment returning a copy of the alignment
-            via getAlignment() -
+        """Run author vs reference sequence alignment returning a copy of the alignment
+        via getAlignment() -
         """
-        logger.info("\n------------------------ ")
+        logger.info("------------------------")
         try:
             pA = PairwiseAlign()
             pA.setVerbose(self.__verbose)
@@ -64,28 +62,28 @@ class PairwiseAlignTests(unittest.TestCase):
             pA.addTestSequence(self.seqAuth3L, "A")
             pA.doAlign()
             pA.prAlignmentConflicts("A")
-            logger.info("Length of reference sequence = %d\n" % len(self.seqRef3L))
-            logger.info("Length of    author sequence = %d\n" % len(self.seqAuth3L))
+            logger.info("Length of reference sequence = %d", len(self.seqRef3L))
+            logger.info("Length of    author sequence = %d", len(self.seqAuth3L))
             myAlign = pA.getAlignment("A")
-            logger.info("Length   of alignment     = %d\n" % len(myAlign))
+            logger.info("Length   of alignment     = %d", len(myAlign))
             ii = 0
             jj = 0
             for myPr in myAlign:
                 if myPr[0] != myPr[1]:
-                    logger.debug("Py - conflict at alignment position %d  -  %s - %s\n" % (ii, myPr[0], myPr[1]))
+                    logger.debug("Py - conflict at alignment position %d  -  %s - %s", ii, myPr[0], myPr[1])
                     jj += 1
                 ii += 1
-            logger.info("Conflicts in alignment %d" % jj)
+            logger.info("Conflicts in alignment %d", jj)
             self.assertGreaterEqual(jj, 80)
         except Exception as e:
-            logger.exception("Failing with %s" % str(e))
+            logger.exception("Failing with %s", str(e))
             self.fail()
 
     def testAlign3(self):
-        """  Consensus alignment for author and reference sequences.
-             Returning the name of any sequence that is not part of the consensus.
+        """Consensus alignment for author and reference sequences.
+        Returning the name of any sequence that is not part of the consensus.
         """
-        logger.info("\n------------------------ ")
+        logger.info("------------------------ ")
         try:
             pA = PairwiseAlign()
             pA.setVerbose(self.__verbose)
@@ -93,67 +91,63 @@ class PairwiseAlignTests(unittest.TestCase):
             pA.addTestSequence(self.seqAuth3L, "A")
             #
             myFails = pA.doAlignConsensus()
-            logger.info("Failed sequences = %d\n" % len(myFails))
+            logger.info("Failed sequences = %d", len(myFails))
             pA.prAlignmentConflicts("A")
-            logger.info("Length reference sequence = %d\n" % len(self.seqRef3L))
-            logger.info("Length    author sequence = %d\n" % len(self.seqAuth3L))
+            logger.info("Length reference sequence = %d", len(self.seqRef3L))
+            logger.info("Length    author sequence = %d", len(self.seqAuth3L))
             myAlign = pA.getAlignment("A")
-            logger.info("Length   of alignment     = %d\n" % len(myAlign))
+            logger.info("Length   of alignment     = %d", len(myAlign))
             ii = 0
             jj = 0
             for myPr in myAlign:
                 if myPr[0] != myPr[1]:
-                    logger.debug("Py - conflict position %8d  -  %3s - %3s\n" % (ii, myPr[0], myPr[1]))
+                    logger.debug("Py - conflict position %8d  -  %3s - %3s", ii, myPr[0], myPr[1])
                     jj += 1
                 ii += 1
-            logger.info("Conflicts in alignment %d" % jj)
+            logger.info("Conflicts in alignment %d", jj)
             self.assertGreaterEqual(jj, 80)
         except Exception as e:
-            logger.exception("Failing with %s" % str(e))
+            logger.exception("Failing with %s", str(e))
             self.fail()
 
     def testAlign4(self):
-        """  Consensus alignment for reference and 5 test sequences
-
-        """
-        logger.info("\n------------------------ ")
+        """Consensus alignment for reference and 5 test sequences"""
+        logger.info("------------------------")
         try:
             pA = PairwiseAlign()
             pA.setVerbose(self.__verbose)
             pA.setReferenceSequence(self.seqRef3L, "REFA")
             for k, v in self.sTests.items():
-                logger.debug("Added test sequence %10s len = %8d\n" % (k, len(v)))
+                logger.debug("Added test sequence %10s len = %8d", k, len(v))
                 pA.addTestSequence(v, k)
             #
             myFails = pA.doAlignConsensus()
-            logger.info("Failed sequences = %d\n" % len(myFails))
-            logger.info("Length reference sequence = %d\n" % len(self.seqRef3L))
+            logger.info("Failed sequences = %d", len(myFails))
+            logger.info("Length reference sequence = %d", len(self.seqRef3L))
             self.assertEqual(len(myFails), 0)
             #
         except Exception as e:
-            logger.exception("Failing with %s" % str(e))
+            logger.exception("Failing with %s", str(e))
             self.fail()
 
     def testAlign5(self):
-        """  Consensus alignment for reference and 5 test sequences
-
-        """
-        logger.info("\n------------------------ ")
+        """Consensus alignment for reference and 5 test sequences"""
+        logger.info("------------------------ ")
         try:
             pA = PairwiseAlign()
             pA.setVerbose(self.__verbose)
             pA.setReferenceSequence(self.seqRef3L, "REFA")
-            logger.info("Length reference sequence = %d\n" % len(self.seqRef3L))
+            logger.info("Length reference sequence = %d", len(self.seqRef3L))
             #
             for k, v in self.sTests.items():
-                logger.info("Added test sequence %10s len = %8d\n" % (k, len(v)))
+                logger.info("Added test sequence %10s len = %8d", k, len(v))
                 pA.addTestSequence(v, k)
             #
             myFails = pA.doAlignConsensus()
-            logger.info("Failed sequences = %d\n" % len(myFails))
+            logger.info("Failed sequences = %d", len(myFails))
             self.assertEqual(len(myFails), 0)
         except Exception as e:
-            logger.exception("Failing with %s" % str(e))
+            logger.exception("Failing with %s", str(e))
             self.fail()
 
 
@@ -167,8 +161,7 @@ def pairAlignSuite():
     return suiteSelect
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     #
-    if True:
-        mySuite = pairAlignSuite()
-        unittest.TextTestRunner(verbosity=2).run(mySuite)
+    mySuite = pairAlignSuite()
+    unittest.TextTestRunner(verbosity=2).run(mySuite)
