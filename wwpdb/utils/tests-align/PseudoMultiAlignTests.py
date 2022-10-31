8f14918788f66f234f74df4411eb0aaaba9285cb
Ezra Peisach <ezra.peisach@rcsb.org>
2022-10-31 13:35:20 -0400

    Bring in new test code to run tests for PsedoMultiAlign

--- wwpdb/utils/tests-align/PseudoMultiAlignTests.py
+++ wwpdb/utils/tests-align/PseudoMultiAlignTests.py
@@ -1,58 +1,60 @@
 import string
-from wwpdb.utils.align.alignlib import PseudoMultiAlign
+import unittest
+from wwpdb.utils.align.alignlib import PseudoMultiAlign  # pylint: disable=no-name-in-module
+
 
 class PseudoMultiAlignTest(object):
     def __init__(self):
         self.__authSeq = [
-             ( "A", "0" ),
-             ( "A", "1" ),
-             ( "A", "2" ),
-             ( "G", "3" ),
-             ( "C", "4" ),
-             ( "A", "5" ),
-             ( "A", "6" ),
-             ( "G", "7" ),
-             ( "C", "8" ),
-             ( "G", "9" ),
-             ( "G", "10"),
-             ( "G", "11"),
-             ( "C", "12"),
-             ( "C", "13"),
-             ( "G", "14"),
-             ( "C", "15"),
-             ( "A", "16"),
-             ( "C", "17"),
-             ( "G", "18"),
-             ( "C", "19"),
-             ( "G", "20"),
-             ( "G", "21"),
-             ( "C", "22"),
-             ( "C", "23"),
-             ( "C", "24"),
-             ( "G", "25"),
-             ( "C", "26"),
-             ( "A", "27"),
-             ( "A", "28"),
-             ( "A", "29")
+            ( "A", "0" ),
+            ( "A", "1" ),
+            ( "A", "2" ),
+            ( "G", "3" ),
+            ( "C", "4" ),
+            ( "A", "5" ),
+            ( "A", "6" ),
+            ( "G", "7" ),
+            ( "C", "8" ),
+            ( "G", "9" ),
+            ( "G", "10"),
+            ( "G", "11"),
+            ( "C", "12"),
+            ( "C", "13"),
+            ( "G", "14"),
+            ( "C", "15"),
+            ( "A", "16"),
+            ( "C", "17"),
+            ( "G", "18"),
+            ( "C", "19"),
+            ( "G", "20"),
+            ( "G", "21"),
+            ( "C", "22"),
+            ( "C", "23"),
+            ( "C", "24"),
+            ( "G", "25"),
+            ( "C", "26"),
+            ( "A", "27"),
+            ( "A", "28"),
+            ( "A", "29")
         ]
         self.__coorSeq = [
-             ( "A", "0",  "1", "648"  ),
-             ( "A", "1",  "1", "649"  ),
-             ( "A", "2",  "1", "650"  ),
-             ( "G", "3",  "1", "651"  ),
-             ( "C", "4",  "1", "652"  ),
-             ( "A", "5",  "0", "652A" ),
-             ( "G", "6",  "1", "652C" ),
-             ( "C", "7",  "1", "652D" ),
-             ( "G", "8",  "1", "652E" ),
-             ( "G", "9",  "0", "652F" ),
-             ( "C", "10", "1", "652S" ),
-             ( "C", "11", "1", "652T" ),
-             ( "G", "12", "1", "652U" ),
-             ( "C", "13", "1", "652V" ),
-             ( "A", "14", "1", "653"  ),
-             ( "A", "15", "1", "654"  ),
-             ( "A", "16", "1", "655"  )
+            ( "A", "0",  "1", "648"  ),
+            ( "A", "1",  "1", "649"  ),
+            ( "A", "2",  "1", "650"  ),
+            ( "G", "3",  "1", "651"  ),
+            ( "C", "4",  "1", "652"  ),
+            ( "A", "5",  "0", "652A" ),
+            ( "G", "6",  "1", "652C" ),
+            ( "C", "7",  "1", "652D" ),
+            ( "G", "8",  "1", "652E" ),
+            ( "G", "9",  "0", "652F" ),
+            ( "C", "10", "1", "652S" ),
+            ( "C", "11", "1", "652T" ) ,
+            ( "G", "12", "1", "652U" ),
+            ( "C", "13", "1", "652V" ),
+            ( "A", "14", "1", "653"  ),
+            ( "A", "15", "1", "654"  ),
+            ( "A", "16", "1", "655"  )
         ]
         self.__seqA = """GUCAAGAUGGUAAGGGCCCACGGUGGAUGCCUCGGCACCCGAGCCGAUGAAGGACGUGGCUACCUGCGAUAAGCCAGGGG
         GAGCCGGUAGCGGGCGUGGAUCCCUGGAUGUCCGAAUGGGGGAACCCGGCCGGCGGGAACGCCGGUCACCGCGCUUUUGC
@@ -100,7 +102,7 @@ class PseudoMultiAlignTest(object):
         alignIndexList = pA.getAlignIndices()
         alignSeqList = pA.getAlignSequences()
         #
-        for idx,alignIdx in enumerate(alignIndexList):
+        for idx, alignIdx in enumerate(alignIndexList):
             sym = "---"
             if (alignSeqList[idx][0] != ".") and (alignSeqList[idx][0] == alignSeqList[idx][1]):
                 sym = "==="
@@ -117,7 +119,7 @@ class PseudoMultiAlignTest(object):
         alignSeqList = pA.getAlignSequences()
         #
         print("")
-        for idx,alignIdx in enumerate(alignIndexList):
+        for idx, alignIdx in enumerate(alignIndexList):
             sym = "---"
             if (alignSeqList[idx][0] != ".") and (alignSeqList[idx][0] == alignSeqList[idx][1]):
                 sym = "==="
@@ -132,15 +134,23 @@ class PseudoMultiAlignTest(object):
             if ss in string.whitespace:
                 continue
             #
-            sL.append( (ss, str(count) ) )
+            sL.append((ss, str(count)))
             count += 1
         #
         return sL
 
-def __main():
+class PseudoMultiAlignUnitTest(unittest.TestCase):
+    def testRun(self):
+        pM = PseudoMultiAlignTest()
+        pM.runTest1()
+        pM.runTest2()
+        
+
+def __main():  # pragma: no cover
     pM = PseudoMultiAlignTest()
     pM.runTest1()
     pM.runTest2()
 
-if __name__ == "__main__":
+
+if __name__ == "__main__":  # pragma: no cover
     __main()
